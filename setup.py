ed2ee699fa7f7c5a336033149ebe07a6ffe5ffd8
nakt <nakt_azdim@walkure.net>
2018-01-05 00:33:52 +0900

    [add] setup.py の内容を記述し、パッケージとしてインストールできるように

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='lr2irscraper',
+    version='0.1',
+    description='LR2 Internet Ranking Scraper Library',
+    author='nakt',
+    author_email='nakt_azdim@walkure.net',
+    install_requires=['pandas'],
+    python_requires='>=3.5',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
+)
