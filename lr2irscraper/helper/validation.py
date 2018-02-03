0c5169e04578c34fa99322e8f905ead6cd471085
nakt <nakt_azdim@walkure.net>
2018-02-03 13:54:36 +0900

    [fix] validation.py のエラーメッセージの誤りを修正

--- lr2irscraper/helper/validation.py
+++ lr2irscraper/helper/validation.py
@@ -9,14 +9,14 @@ def validate_id(bmsid: int):
 
 def validate_hash(hash_value: str):
     if not re.match("^([0-9a-f]{32}|[0-9a-f]{160})$", hash_value):
-        raise ValueError("{}: hash must be 32 or 160 hexadecimal digits".format(hash))
+        raise ValueError("{}: hash must be 32 or 160 hexadecimal digits".format(hash_value))
 
 
 def validate_bms_hash(hash_value: str):
     if not re.match("^[0-9a-f]{32}$", hash_value):
-        raise ValueError("{}: BMS hash must be 32 hexadecimal digits".format(hash))
+        raise ValueError("{}: BMS hash must be 32 hexadecimal digits".format(hash_value))
 
 
 def validate_course_hash(hash_value: str):
     if not re.match("^[0-9a-f]{160}$", hash_value):
-        raise ValueError("{}: course hash must be 160 hexadecimal digits".format(hash))
+        raise ValueError("{}: course hash must be 160 hexadecimal digits".format(hash_value))
