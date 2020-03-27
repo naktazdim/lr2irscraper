59c7ec4029ecca64050943c8ad57d9a40bf55833
nakt <nakt_azdim@walkure.net>
2020-03-27 20:28:37 +0900

    [delete] UnregisteredErrorを削除
    使っていなかったので

--- lr2irscraper/helper/exceptions.py
+++ lr2irscraper/helper/exceptions.py
@@ -1,17 +1,6 @@
-from lr2irscraper.types import BmsMd5
-
-
 class ParseError(Exception):
     def __init__(self, message: str):
         self.message = message
 
     def __str__(self):
         return self.message
-
-
-class UnregisteredError(Exception):
-    def __init__(self, bmsmd5: BmsMd5):
-        self.bmsmd5 = bmsmd5
-
-    def __str__(self):
-        return "unregistered bmsmd5: {}".format(self.bmsmd5.hash)
