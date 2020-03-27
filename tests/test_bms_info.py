3ff0675732ac8ccd16c1d92f3efdc5591b1665ee
nakt <nakt_azdim@walkure.net>
2020-03-27 21:28:22 +0900

    [add] BmsInfoのテストを書いた

---
+++ tests/test_bms_info.py
@@ -0,0 +1,18 @@
+import pytest
+
+from lr2irscraper import BmsInfo
+from tests.util import resource
+
+
+@pytest.mark.parametrize("source,bms_info", [
+    (resource("bms_info/test.html"), BmsInfo(type="bms", lr2_id=15, title="星の器～STAR OF ANDROMEDA (ANOTHER)")),
+    (resource("bms_info/test_course.html"), BmsInfo(type="course", lr2_id=11099, title="GENOSIDE 2018 段位認定 Overjoy")),
+])
+def test_bms_info(source, bms_info):
+    # とりあえず読めることを確認
+    assert BmsInfo.from_source(source) == bms_info
+
+
+def bms_info_unregistered():
+    bms_info = BmsInfo.from_source(resource("bms_info/test_unregistered.html"))
+    assert bms_info is None
