ecd2282b82f50ded5c87378ae25d030180dc6b77
nakt <nakt_azdim@walkure.net>
2020-03-27 12:59:28 +0900

    [refactor] 不要コード削除

--- lr2irscraper/__init__.py
+++ lr2irscraper/__init__.py
@@ -1,3 +1,3 @@
 from .bms_table import get_bms_table
-from .ranking import get_ranking_data, get_ranking_data_detail
+from .ranking import get_ranking_data
 from .hash_util import hash_to_id, hash_to_bmsid, hash_to_courseid, courseid_to_hash
--- lr2irscraper/ranking.py
+++ lr2irscraper/ranking.py
@@ -3,7 +3,6 @@ from time import sleep
 from lr2irscraper.helper.fetch import *
 from lr2irscraper.helper.data_extraction.ranking import *
 from lr2irscraper.helper.validation import *
-from lr2irscraper.helper.exceptions import UnregisteredError
 
 
 def get_ranking_data(hash_value: str) -> pd.DataFrame:
@@ -21,50 +20,3 @@ def get_ranking_data(hash_value: str) -> pd.DataFrame:
     """
     validate_hash(hash_value)
     return extract_ranking_from_xml(fetch_ranking_xml(hash_value))
-
-
-def get_ranking_data_detail(id_or_hash: Union[int, str], mode: str, interval: float=1.0) -> pd.DataFrame:
-    """ 詳細なランキングデータを取得する。bmsid (courseid) およびハッシュ値のいずれを渡してもよい。
-
-    1 ページ (100 件) ずつランキングページを読み取ってデータを取得する。
-    get_ranking_data() より多くの情報が得られるが、低速。取得中にランキングが更新されてしまい、データに抜け・重複が生じることがある。
-    ハッシュ値がわかっていて get_ranking_data() のデータで事足りる場合は極力そちらを使用すること。
-
-    Args:
-        id_or_hash: bmsid, courseid またはハッシュ値
-        mode: "bmsid", "courseid", "hash" のいずれかを指定
-        interval: 1ページ取得するごとに interval 秒だけ間隔をあける (サーバに負荷をかけすぎないため)
-
-    Returns:
-        ランキングデータ
-        (id, rank, name, sp_dan, dp_dan, clear, dj_level, score, max_score, score_percentage,
-         combo, notes, minbp, pg, gr, gd, bd, pr, gauge_option, random_option, input, body, comment)
-
-    """
-    if mode in ["bmsid", "courseid"]:
-        validate_id(id_or_hash)
-    elif mode == "hash":
-        validate_hash(id_or_hash)
-    else:
-        raise ValueError("{}: mode must be 'bmsid', 'courseid' or 'hash'".format(mode))
-
-    # まず 1 ページ目を取得し、そこから諸々の情報を得る
-    source = fetch_ranking_html(id_or_hash, mode, 1)
-
-    if chart_unregistered(source):
-        raise UnregisteredError(id_or_hash, mode)
-
-    player_count = read_player_count_from_html(source)  # プレイヤ数を取得
-    page_count = (player_count + 99) // 100  # プレイヤ数を 100 で割って切り上げるとページ数
-
-    data_frames = [extract_ranking_from_html(source)]  # 以下でここに各ページを DataFrame に変換したものを格納
-
-    # 2 ページ目以降を順に取得
-    for page in range(2, page_count + 1):
-        sleep(interval)
-
-        source = fetch_ranking_html(id_or_hash, mode, page)
-        data_frames.append(extract_ranking_from_html(source))
-
-    return pd.concat(data_frames)  # 全ページのデータを結合して返す
-
